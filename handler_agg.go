64276d77fac279baff2fa63050aa11c2ec07e52e
Vikuuu <adivik672@gmail.com>
2024-10-29 14:05:15 +0530

    build: add browse command
    Add command 'browse' that takes 'limit' as its optional parameter and
    shows the recently fetched posts in the terminal, if limit is not given
    it will show 2 recent posts.

--- handler_agg.go
+++ handler_agg.go
@@ -4,10 +4,39 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"log"
+	"strconv"
 	"time"
+
+	"github.com/google/uuid"
 
 	"github.com/Vikuuu/gator/internal/database"
 )
+
+func handlerBrowse(s *state, cmd command) error {
+	args := 2
+	if len(cmd.Args) == 1 {
+		l, err := strconv.Atoi(cmd.Args[0])
+		if err != nil {
+			return fmt.Errorf("error <limit> should be a number: %w", err)
+		}
+
+		args = l
+	}
+
+	posts, err := s.db.GetPostsForUser(context.Background(), args)
+	if err != nil {
+		return fmt.Errorf("error while getting posts for user: %w", err)
+	}
+
+	for _, post := range posts {
+		fmt.Printf("Title: %s\n", post.Title)
+		fmt.Printf("Pub at: %s\n", post.PublishedAt)
+		fmt.Printf("Description: %s\n\n", post.Description)
+	}
+
+	return nil
+}
 
 func handlerAgg(s *state, cmd command) error {
 	if len(cmd.Args) != 1 {
@@ -24,6 +53,7 @@ func handlerAgg(s *state, cmd command) error {
 	ticker := time.NewTicker(timeBetweenReq)
 	for ; ; <-ticker.C {
 		scrapeFeeds(s)
+		fmt.Println("Feed Scraped")
 	}
 }
 
@@ -53,13 +83,31 @@ func scrapeFeeds(s *state) error {
 		return fmt.Errorf("error fetching the feed: %w", err)
 	}
 
-	printRssFeed(rssFeed)
+	saveInPosts(s, rssFeed, feed.ID)
 	return nil
 }
 
-func printRssFeed(rssFeed *RSSFeed) {
-	fmt.Printf("Title      : %s\n", rssFeed.Channel.Title)
+func saveInPosts(s *state, rssFeed *RSSFeed, feedID uuid.UUID) error {
 	for _, item := range rssFeed.Channel.Item {
-		fmt.Printf(" - Title 	   : %s\n", item.Title)
+		layout := "Mon, 02 Jan 2006 15:04:05 -0700"
+		t, err := time.Parse(layout, item.PubDate)
+		if err != nil {
+			log.Fatalf("Error Parsing time %v: ", err)
+		}
+
+		err = s.db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Url:         sql.NullString{String: item.Link, Valid: true},
+			Description: item.Description,
+			PublishedAt: t,
+			FeedID:      feedID,
+		})
+		if err != nil {
+			return fmt.Errorf("error saving posts: %w", err)
+		}
 	}
+	return nil
 }
